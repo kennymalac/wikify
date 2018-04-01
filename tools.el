cec332e344e5df522f21027e80872ecf27f4cfbc
Kenneth Malac <kennethm777@gmail.com>
2018-03-31 20:58:29 -0700

    Working alpha version, the formatting is crap in the exported though.

--- tools.el
+++ tools.el
@@ -1,39 +1,46 @@
-(eval-when-compile (require 'generator))
+;; -*- lexical-binding: t -*-
+(require 'json)
+(require 'cl-lib)
+(require 'generator)
 
 
 (defun tab-has-keywords-p (keywords tab)
-  (let ((ks apply #'downcase (gethash "keywords")) (tab-title (gethash "title" tab)))
+  (let ((ks (mapcar 'downcase keywords)) (tab-title (gethash "title" tab)))
     (seq-find (lambda (keyword)
                 (cl-search keyword tab-title))
               ks)))
 
 
 (defun tab-has-domains-p (domains tab)
-  (let (tab-domain (gethash "domain" tab))
+  (let ((tab-url (gethash "url" tab)))
     (seq-find (lambda (domain)
-                (string-match-p (regexp-quote (format "http|https\:\/\/(?:www)?%s(?:\/?.*)?" domain) tab-domain)))
+                (string-match-p (regexp-quote (format "http|https\:\/\/(?:www)?%s(?:\/?.*)?" domain)) tab-url))
               domains)))
 
 (defun tab-has-urlpattern-p (urlpatterns tab)
-  (nil))
+  ;; TODO
+  nil)
 
 
 (iter-defun categorize-from-tabs (categories tabs)
-  ;(setq reduced copy-hash-table categories)
-  (defvar uncategorized-tabs (copy-sequence tabs))
+                                        ;(setq reduced copy-hash-table categories)
+  (message "%s" categories)
+  (let ((uncategorized-tabs (copy-sequence tabs)))
+    (cl-labels ((filter-tabs (category)
+                             ;;(message "%s" category)
+                             (seq-filter
+                              (lambda (tab)
+                                (or (tab-has-keywords-p (gethash "keywords" category) tab)
+                                    (tab-has-domains-p (gethash "domains" category) tab)
+                                    (tab-has-urlpattern-p (gethash "urlpatterns" category) tab)))
+                              uncategorized-tabs)))
+      ;;(puthash category-tabs-stack)
 
-  (labels ((filter-tabs (category)
-                        (seq-remove
-                         (lambda (tab)
-                           (or (tab-has-keywords-p (gethash "keywords" category) tab)
-                               (tab-has-domains-p (gethash "domains" category) tab)
-                               (tab-has-urlpattern-p (gethash "urlpattern" category) tab)))
-                         (uncategorized-tabs))))
-                        ;;(puthash category-tabs-stack)
+      ;; Take each category, yield its tabs
+       (dolist (category categories)
+         (iter-yield (cons category (filter-tabs category)))
 
-    ;; Take each category, yield its tabs
-    (dolist (category categories)
-      (iter-yield (category (filter-tabs category))))))
+           ))))
 
 ;; (defun define-tag)
 
@@ -42,23 +49,34 @@
 ;; (defun deconstruct-window ())
 
 (defun html-block-from-category (category)
-  (let (name (gethash "name" category))
-    (insert (format "#+ATTR_HTML: :title %s\n%s\n%s"
-                    (name) (name) (gethash "keywords" category)))))
+  (let ((name (gethash "name" category)))
+    ;; #+ATTR_HTML: :title %s\n
+    (insert (format "%s\n%s\n"
+                    name (gethash "keywords" category)))))
 
 (defun html-block-from-tab (tab)
-  (insert (format "#+ATTR_HTML: :title %s\n[[%s]] [[%s]]"
-                  (gethash "title" tab) (gethash "favIconUrl" tab) (gethash "url" tab))))
+  (let ((title (gethash "title" tab "")))
+    (insert (format "[[file:%s]]\n#+ATTR_HTML: :title %s\n[[%s][%s]]\n"
+                    (gethash "favIconUrl" tab "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGS6r69tiH8bFQLHVOlhXcmCvIkcbCIYmZEiZRY8a7ws5jKZbXqw") title (gethash "url" tab "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGS6r69tiH8bFQLHVOlhXcmCvIkcbCIYmZEiZRY8a7ws5jKZbXqw") title))))
 
 
 
 ;; Complex parsing comes later?
 (defun parse-config (config-data)
-  (let (parsed-data (json-read-string-from-string config-data))
+  (setq lexical-binding nil)
+  (let* ((json-object-type 'hash-table)
+         (json-array-type 'list)
+         (json-key-type 'string)
+         (parsed-data (json-read-file config-data)))
     ;; TODO windows . (gethash Windows parsed-data)
-    (return ((categories . (gethash "Categories" parsed-data)) ))))
+    `((categories . ,(gethash "Categories" parsed-data)) (windows . windows))))
 
 (defun parse-tabs (tabs-data)
-  (return (json-read-string-from-string tabs-data))) ;; TODO parse-open-windows..
+  (setq lexical-binding nil)
+  (let* ((json-object-type 'hash-table)
+         (json-array-type 'list)
+         (json-key-type 'string)
+         (parsed-data (json-read-file tabs-data)))
+    parsed-data)) ;; TODO parse-open-windows..
 
 
--- wiki.el
+++ wiki.el
@@ -1,11 +1,16 @@
-(eval-when-compile (require 'cl 'generator))
+(load-file "./tools.el")
+(require 'cl)
+(require 'generator)
 
 
 (defun org-wiki-generate (tabs-json config-json)
-  (let* ((tabs (parse-tabs tabs-json)) (config (parse-config config-json)) (categories (assoc 'categories config))))
+  (let* ((tabs (parse-tabs tabs-json)) (config (parse-config config-json)) (categories (alist-get 'categories config)))
     (iter-do (val (categorize-from-tabs categories tabs))
-             (cl-destructuring-bind (category filtered-tabs) 'val
-               (html-block-from-category category)
-               (cl-loop for tab in (filtered-tabs) do (html-block-from-tab tab) do (forward-line 1)))))
+      (let ((category (car val)) (filtered-tabs (cdr val)))
+        (html-block-from-category category)
+        (cl-loop for tab in filtered-tabs do (html-block-from-tab tab))
+        ))))
 
 ;;;(dotimes ((html-block-from-bookmark (bookmark)))
+
+;;(org-wiki-generate "../../../org/tabs.json" "categories_example.json")
